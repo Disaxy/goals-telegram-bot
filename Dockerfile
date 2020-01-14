fd8f9e453a7b353702df0b3b1ea8c32ac673ae9d
disaxy <gdemoinap@gmail.com>
2020-01-14 16:30:59 +0300

    code review

--- Dockerfile
+++ Dockerfile
@@ -1,12 +1,14 @@
 FROM python:3.8-alpine
 
-WORKDIR /home
+WORKDIR /bot
+
+COPY *.py, requirements.txt, *.sqlite, *.data ./
 
 ENV TZ=Europe/Moscow
 
-RUN ln -snf /usr/share/zoneinfo/$TZ /etc/localtime && echo $TZ > /etc/timezone
-RUN pip install -Ur requairements.txt && apt-get update && apt-get install sqlite3
-
-COPY *.py, requairements.txt, *.sqlite, *.data ./
+RUN ln -snf /usr/share/zoneinfo/$TZ /etc/localtime && \
+    echo $TZ > /etc/timezone && \
+    pip install -Ur requirements.txt && \
+    apt-get update && apt-get install sqlite3
 
 ENTRYPOINT ["python", "bot.py"]
--- db.py
+++ db.py
@@ -4,6 +4,7 @@ import asyncio
 from typing import NamedTuple
 
 from aiosqlite import connect
+from asyncinit import asyncinit
 
 
 class Expense(NamedTuple):
@@ -12,10 +13,29 @@ class Expense(NamedTuple):
     comment: str = None
 
 
+
+@asyncinit
 class DB:
 
-    def __init__(self, db_path):
+    async def __init__(self, db_path, init_path):
         self.db_path = db_path
+        self.init_path = init_path
+        await self.__check_db()
+    
+    async def __check_db(self):
+        async with connect(self.db_path) as db:
+            async with db.execute('SELECT name FROM `sqlite_master` WHERE type="table" AND name="expense"') as cursor:
+                table_exists = await cursor.fetchall()
+                if not table_exists:
+                    await self.__initialize()
+
+    async def __initialize(self):
+        with open(self.init_path, 'r', encoding='utf-8') as file:
+            sql = file.read()
+
+        async with connect(self.db_path) as db:
+            await db.executescript(sql)
+            await db.commit()
 
     async def view_expense(self):
         async with connect(self.db_path) as db:
@@ -32,7 +52,7 @@ class DB:
 
     async def create_expense(self, expense: Expense):
         async with connect(self.db_path) as db:
-            await db.execute('INSERT INTO `expense` (category, amount, comment, date) VALUES (?, ?, ?, datetime("now", "localtime"))', (expense.category, expense.amount, expense.comment,))
+            await db.execute('INSERT INTO `expense` (category_id, amount, comment, created) VALUES ((SELECT id FROM `category` WHERE name = ?), ?, ?, datetime("now", "localtime"))', (expense.category, expense.amount, expense.comment,))
             await db.commit()
 
     async def remove_expense(self, expense_id: int):
@@ -50,12 +70,15 @@ async def main():
     import os
     db_path = os.path.join(os.path.dirname(
         os.path.abspath(__file__)), '.sqlite')
-    db = DB(db_path)
-    # await db.create_expense(Expense(category='Питание', amount=100))
+    init_path = os.path.join(os.path.dirname(
+        os.path.abspath(__file__)), 'init.sql')
+    db = await DB(db_path, init_path)
+    # await db.check_db()
+    await db.create_expense(Expense(category='Продукты', amount=100))
     # await db.remove_expense(5)
     # await db.remove_last_expense()
-    # await db.view_expense()
-    await db.last_day()
+    await db.view_expense()
+    # await db.last_day()
 
 if __name__ == '__main__':
     asyncio.run(main())
